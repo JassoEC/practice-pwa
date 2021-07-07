33a45dd01427537b0ac596641b0a4b9493405fdb
Emanuel Campos Jasso <emanuel.campos@sulivan.com.mx>
2021-07-07 09:21:56 -0500

    ejemplo de manipulacion de respuestas mediante el sw

--- 02-service-worker/sw.js
+++ 02-service-worker/sw.js
@@ -1,7 +1,18 @@
+// De esta forma se interceptan y se manipulan las respuestas mediante un SW
+
 self.addEventListener('fetch', event => {
   if (event.request.url.includes('style.css')) {
-    event.respondWith(null)
-  } else {
-    event.respondWith(fetch(event.request));
+    // resultado de cualquier peticion fetch
+    let response = new Response(`
+      body {
+        background-color: red !important;
+        color:pink;
+      }
+    `, {
+      headers: {
+        'Content-Type': 'text/css'
+      }
+    });
+    event.respondWith(response)
   }
 })
