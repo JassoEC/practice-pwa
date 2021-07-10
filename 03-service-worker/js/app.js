2de4ae282a8319185e6d44f658167d218fb79e53
Emanuel Campos Jasso <emanuel.campos@sulivan.com.mx>
2021-07-10 09:50:19 -0500

    se agregaron los ejemplos de listeners comunes para las pwa

--- 03-service-worker/js/app.js
+++ 03-service-worker/js/app.js
@@ -1,4 +1,21 @@
 // Detectar si podemos usar Service Workers
 if (navigator.serviceWorker) {
-    navigator.serviceWorker.register('/sw.js');
+    navigator.serviceWorker.register('/sw.js')
+        .then(reg => {
+            setTimeout(() => {
+                reg.sync.register('posteo-de-gatos');
+                console.log('Se mandaron fotos de gatos, ay que bonito');
+            }, 3000)
+        });
 }
+
+/* if(){
+
+} */
+
+const makeRequets = () => fetch('https://reqres.in/api/users')
+    .then(resp => resp.text())
+    .then(console.log);
+
+
+// makeRequets();
--- 03-service-worker/sw.js
+++ 03-service-worker/sw.js
@@ -7,14 +7,31 @@ self.addEventListener('install', event => {
   const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
       console.log('instalacionres realizadas');
+      self.skipWaiting(promise);
       resolve();
-    }, [1000])
+    })
   })
 
-  self.skipWaiting(promise);
 })
 
 
 self.addEventListener('activate', event => {
   console.log('se activo SW. Listo para controlar app, le cae encima al anterior');
+})
+
+self.addEventListener('fetch', event => {
+  // aplicar estrategias de cache
+  /* console.log(`SW: ${event.request.url}`);
+  if (event.request.url.includes('https://reqres.in/')) {
+    const response = new Response('{ok: false, message: "jajaja"}')
+    event.respondWith(response)
+  } */
+});
+
+
+// Cuando se recupera la conexion a internet
+self.addEventListener('sync', event => {
+  console.log('Tenemos conexion');
+  console.log(event);
+  console.log(event.tag);
 })
