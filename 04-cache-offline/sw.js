c14eb8e23dc1c528145ac245a82ad313a2c5bb1a
Emanuel Campos Jasso <emanuel.campos@sulivan.com.mx>
2021-07-13 09:46:51 -0500

    se agrego el ejemplo de respuesta de no conexion a internet

--- 04-cache-offline/sw.js
+++ 04-cache-offline/sw.js
@@ -1,3 +1,9 @@
+self.addEventListener('fetch', event => {
 
+  const offLineResponse = new Response(`Bievenido  a mi pagina web, conectate a internet`)
 
+  const response = fetch(event.request)
+    .catch(() => offLineResponse)
 
+  event.respondWith(response)
+})
