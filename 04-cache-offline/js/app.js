c3ed47edc8a100cba1a500680f8d5d64309136aa
Emanuel Campos Jasso <emanuel.campos@sulivan.com.mx>
2021-07-13 10:38:43 -0500

    se agreagaron los ejmplos de escritura lectura y manipulacion de los elementos del cache

--- 04-cache-offline/js/app.js
+++ 04-cache-offline/js/app.js
@@ -1,5 +1,37 @@
-
-
-if ( navigator.serviceWorker ) {
+if (navigator.serviceWorker) {
     navigator.serviceWorker.register('/sw.js');
+}
+
+if (window.caches) {
+    // es un espacio del navegador en el hdd
+    caches.open('prueba-1')
+    caches.open('prueba-2')
+
+    // verificar si existe un cache
+    // caches.has('prueba-2').then(console.log);
+
+    // Borrar cache depues de usarlo
+
+    // caches.delete('prueba-1').then(console.log)
+
+    caches.open('cache-v1.1').then(cache => {
+        // cache.add('/index.html');
+        cache.addAll([
+            '/index.html',
+            '/css/style.css',
+            '/img/main.jpg'
+        ]).then(() => {
+            /* cache.delete('/css/style.css') */
+            cache.put('index.html', new Response('Hola mundirijillo'))
+        });
+
+
+        /* cache.match('/index.html').then(resp => {
+            resp.text().then(console.log)
+        }) */
+    });
+
+
+    caches.keys().then(keys => console.log(keys))
+
 }
--- 04-cache-offline/sw.js
+++ 04-cache-offline/sw.js
@@ -1,9 +1,32 @@
 self.addEventListener('fetch', event => {
 
-  const offLineResponse = new Response(`Bievenido  a mi pagina web, conectate a internet`)
+  /*const  offLineResponse = new Response(`Bievenido  a mi pagina web, conectate a internet`) */
 
+
+  /*  const offLineResponse = new Response(`
+  <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Mi PWA</title>    
+    
+</head>
+<body class="container p-3">
+<h1>Offline mode</h1>
+</body>
+</html>
+  `, {
+    headers: {
+      'Content-Type': 'text/html'
+    }
+  }); */
+
+
+  const offLineResponse = fetch('pages/offline.html')
   const response = fetch(event.request)
-    .catch(() => offLineResponse)
+    .catch(() => fetch(offLineResponse))
 
   event.respondWith(response)
 })
